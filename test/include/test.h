9c51499955102bad921fd24dfc0ee237d36f456f
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-21 10:39:57 -0500

    Add option CATCH2__TEST, which builds a dummy library with tests and code coverage generation.

---
+++ test/include/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST__H
+#define TEST__H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int add( int a, int b );
+int multiply( int a, int b );
+void say_hello( char *buffer, int buffer_size );
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // TEST__H
